f8376d2ba335271f93f54d20bf28dacb55b6b9e3
TechStudent11 <76978184+TechStudent11@users.noreply.github.com>
2021-03-16 16:31:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,14 @@ from api import api
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix="/api")
 
-# PATH FUNCTION
-def path(rule, function, **options):
-    app.add_url_rule(rule, function.__name__, function, **options) 
-
-# REACT TEMPLATE
-def react(**kwargs):
-    return render_template('index.html', **kwargs)
-
 # VIEWS
+@app.route("/")
 def index():
     return react()
 
+@app.route("/about")
 def about():
     return react()
-
-# ADD VIEWS
-path('/', index)
-path('/about', about)
 
 # RUN FLASK
 if __name__ == "__main__":
